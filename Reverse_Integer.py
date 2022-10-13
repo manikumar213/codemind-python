fbe20dcc307ab9cdef3f4836db893a304b83c319
CHEKKA VEERA VENKATA DURGA MANIKUMAR <113907295+manikumar213@users.noreply.github.com>
2022-10-13 14:03:33 +0530

    n=int(input()) if n<0:     t=-n else:     t=n rev=0 while t>0:     r=t%10     rev=rev*10+r     t=t//10 if n<0:         print(-rev) else:         print(rev)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,14 +1,14 @@
 n=int(input())
-is_negative=0
 if n<0:
-    is_negative=1
-    n=-n
+    t=-n
+else:
+    t=n
 rev=0
-while n>0:
-    r=n%10
+while t>0:
+    r=t%10
     rev=rev*10+r
-    n=n//10
-if is_negative==1:
+    t=t//10
+if n<0:
         print(-rev)
 else:
         print(rev)
