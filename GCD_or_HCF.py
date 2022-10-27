6a67b14c2df4772ae4dfe21e98b12a9197680fef
CHEKKA VEERA VENKATA DURGA MANIKUMAR <113907295+manikumar213@users.noreply.github.com>
2022-10-27 22:44:04 +0530

    a,b=map(int,input().split()) for i in range(1,a+1):     if a%i==0 and b%i==0:             gcd=i print(gcd)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,7 +1,5 @@
 a,b=map(int,input().split())
-while a!=b:
-    if a>b:
-        a-=b
-    else:
-        b-=a
-print(a)   
+for i in range(1,a+1):
+    if a%i==0 and b%i==0:
+            gcd=i
+print(gcd)
