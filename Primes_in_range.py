7d3b5e135a4787eca842702e8b2e9fb8016beee3
CHEKKA VEERA VENKATA DURGA MANIKUMAR <113907295+manikumar213@users.noreply.github.com>
2022-11-07 16:23:55 +0530

    def is_prime(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True m=int(input()) n=int(input()) c=0 for i in range(m,n+1):     if is_prime(i)==True:         c+=1 print(c)

---
+++ Primes_in_range.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+m=int(input())
+n=int(input())
+c=0
+for i in range(m,n+1):
+    if is_prime(i)==True:
+        c+=1
+print(c)
